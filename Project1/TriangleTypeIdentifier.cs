8465a96b0574a2e39ebfb2930ce2be52f63e0546
Albertnoble123 <kobi8583@gmail.com>
2025-08-04 19:00:43 +0000

    Added TriangleTypeIdentifie

---
+++ Project1/TriangleTypeIdentifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Enter side A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter side B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter side C: ");
+        int c = int.Parse(Console.ReadLine());
+
+        if (a == b && b == c)
+            Console.WriteLine("The triangle is Equilateral.");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("The triangle is Isosceles.");
+        else
+            Console.WriteLine("The triangle is Scalene.");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+ 
+}
